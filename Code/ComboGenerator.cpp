cd07f2b2e26b3d3fb33baecafad9603c64bc1b9a
Jeffrey Porzio <jporzio@Virtusa.com>
2019-05-30 15:35:30 -0400

    Starting to do real OO

---
+++ Code/ComboGenerator.cpp
@@ -0,0 +1,53 @@
+#include "ComboGenerator.h"
+
+ComboGenerator::ComboGenerator(int N, int k) {
+
+	this->N = N;
+	this->k = k;
+
+	int* current_combo = new int[k];
+	// initialize first combination
+	for (int i = 0; i < k; i++) {
+		current_combo[i] = i;
+	}
+
+	int i = k - 1;
+
+	bool is_finished = false;
+
+}
+
+ComboGenerator :: ~ComboGenerator() {
+	delete current_combo;
+}
+
+bool ComboGenerator::isFinished() {
+	return this->is_finished;
+}
+
+int* ComboGenerator :: getNextCombo() {
+
+	// a[0] can only be n-r+1 exactly once - our termination condition!
+	if (current_combo[0] < N - k + 1) {
+		// If outer elements are saturated, keep decrementing i till you find unsaturated element
+		while (i > 0 && current_combo[i] == N - k + i) {
+			i--;
+		}
+
+		return current_combo; 
+
+		current_combo[i]++;
+		// Reset each outer element to prev element + 1
+		while (i < k - 1) {
+			current_combo[i + 1] = current_combo[i] + 1;
+			i++;
+		}
+	} else {
+		is_finished = true;
+	}
+
+
+}
+
+void ComboGenerator::restart() {}
+
---
+++ Code/ComboGenerator.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+class ComboGenerator{
+
+public: 
+	ComboGenerator(const int N, const int k);
+	~ComboGenerator();
+
+	bool isFinished();
+	int* getNextCombo();
+	void restart();
+
+
+private: 
+	int N;
+	int k;
+	int i; 
+	int* current_combo; 
+	bool is_finished;
+
+};
